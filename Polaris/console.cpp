646dfa8dd42decb7b0b6a782261c1749addc7137
irma <68775263+omairma@users.noreply.github.com>
2020-08-13 16:43:16 +0200

    Fixed inconsistencies in comments

--- Polaris/console.cpp
+++ Polaris/console.cpp
@@ -4,48 +4,55 @@ HANDLE hConsole;
 
 namespace polaris
 {
-	// log a message to console
+	// Log a message to console
 	void Console::Log(const char* msg)
 	{
 		if(!hConsole)
 			hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
+		// Make the console text cyan to differentiate Polaris from Fortnite logs
 		SetConsoleTextAttribute(hConsole, 11);
 
 		std::cout << "LogPolaris:";
 
+		// Make the console text white again
 		SetConsoleTextAttribute(hConsole, 15);
 
 		std::cout << "Display: " << msg << std::endl;
 	}
 
-	// log an error to console
+	// Log an error to console.
 	void Console::LogError(const char* msg)
 	{
 		if (!hConsole)
 			hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
+		// Make the console text cyan to differentiate Polaris from Fortnite logs
 		SetConsoleTextAttribute(hConsole, 11);
 
 		std::cout << "LogPolaris:";
 
+		// Make the console text red, indicating an error.
 		SetConsoleTextAttribute(hConsole, 12);
 
 		std::cout << "Error: " << msg << std::endl;
 
+		// Make the console text white again
 		SetConsoleTextAttribute(hConsole, 15);
 	}
 
-	// log a message to the console without the Polaris prefix.
+	// Log a message to the console without the Polaris prefix.
 	void Console::LogRaw(const char* msg, int color)
 	{
 		if (!hConsole)
 			hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
+		// Make the console text any color specified by `color`
 		SetConsoleTextAttribute(hConsole, color);
 
 		std::cout << msg << std::endl;
 
+		// Make the console text white again
 		SetConsoleTextAttribute(hConsole, 15);
 	}
 }
