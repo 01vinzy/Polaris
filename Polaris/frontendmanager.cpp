411ce8e053fb41f7051606705fdc0ea74b0ac2c6
irma <68775263+omairma@users.noreply.github.com>
2020-08-16 10:12:32 +0200

    Revert "Taking matters into my own hands, I want to get as much done as I can before school starts."
    This reverts commit 50bd20e2dba7030dd6acf167dd650887b2449362.

---
+++ Polaris/frontendmanager.cpp
@@ -0,0 +1,19 @@
+/*
+ * [irma] This class is unfinished, but I plan to have a system here that loads the main Polaris behaviour when the player readies up
+*/
+
+#include "frontendmanager.h"
+#include "console.h"
+
+namespace polaris 
+{
+	FrontendManager::FrontendManager() 
+	{
+		Console::Log("Starting FrontendManager");
+	}
+
+	FrontendManager::~FrontendManager()
+	{
+
+	}
+}
