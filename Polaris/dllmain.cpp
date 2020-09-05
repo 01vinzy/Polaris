064256feb3cde3cd95e752a22f62b4805d315820
Cyuubi <cyuubiapps@gmail.com>
2020-09-05 15:36:25 -0400

    irma burger

--- Polaris/dllmain.cpp
+++ Polaris/dllmain.cpp
@@ -9,6 +9,7 @@
 #include "common.h"
 #include "athena.h"
 #include "console.h"
+#include "util.h"
 
 // The main thread for Polaris. Called when the Polaris DLL has successfully been injected.
 DWORD WINAPI Main(LPVOID lpParam)
@@ -20,51 +21,32 @@ DWORD WINAPI Main(LPVOID lpParam)
 		ExitProcess(EXIT_FAILURE);
 	}
 
+	// TODO(Cyuubi): Move this into a seperate class for patches.
+	auto pAbilityPatchAddress = polaris::Util::FindPattern
+	(
+		"\xC0\x0F\x84\x3C\x02\x00\x00\x0F\x2F\xF7\x0F\x86\xF5\x00\x00\x00",
+		"xxxxxxxxxxxxxxxx"
+	);
+	if (!pAbilityPatchAddress)
+	{
+		MessageBox(NULL, static_cast<LPCWSTR>(L"An error has occured, please re-open Fortnite and try again!"), static_cast<LPCWSTR>(L"Error"), MB_ICONERROR);
+		ExitProcess(EXIT_FAILURE);
+	}
+
+	DWORD curProtection;
+	VirtualProtect(pAbilityPatchAddress, 16, PAGE_EXECUTE_READWRITE, &curProtection);
+
+	reinterpret_cast<uint8_t*>(pAbilityPatchAddress)[2] = 0x85;
+	reinterpret_cast<uint8_t*>(pAbilityPatchAddress)[11] = 0x8D;
+
+	DWORD temp;
+	VirtualProtect(pAbilityPatchAddress, 16, curProtection, &temp);
+
 	// Start the Polaris modules.
 	new polaris::Console(); // Initialize the debug console.
 	new polaris::Renderer; // Initialize the Polaris renderer.
 
 	new polaris::Athena; // TEMP Start Athena.
-
-	// Test
-	HWND hWnd = FindWindowA("UnrealWindow", "Fortnite ");
-	if (!hWnd)
-	{
-		MessageBox(0, L"Failed to find Fortnite window.", L"Error", MB_ICONERROR);
-		ExitProcess(EXIT_FAILURE);
-	}
-	else
-	{
-		int byte1 = 0x85;
-		int byte2 = 0x8D;
-
-		DWORD PID;
-		GetWindowThreadProcessId(hWnd, &PID);
-		HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, false, PID);
-
-		if (!hProc)
-		{
-			MessageBox(0, L"Cannot open process.", L"Error", MB_ICONERROR);
-			ExitProcess(EXIT_FAILURE);
-		}
-		else
-		{
-			int override1 = WriteProcessMemory(hProc, (LPVOID)0x0028F9A2, &byte1, (DWORD)sizeof(&byte1), NULL);
-			int override2 = WriteProcessMemory(hProc, (LPVOID)0x0028F9AB, &byte2, (DWORD)sizeof(&byte2), NULL);
-
-			if (override1 > 0)
-				polaris::Console::Log("Success 1!");
-			else
-				polaris::Console::LogError("No dice.");
-
-			if (override2 > 0)
-				polaris::Console::Log("Success 2!");
-			else
-				polaris::Console::LogError("No dice.");
-
-			CloseHandle(hProc);
-		}
-	}
 
 	return FALSE;
 }
