f3bc5ef3164d0c1127042da0202e024099c912b1
Cyuubi <cyuubiapps@gmail.com>
2020-09-17 20:20:27 -0400

    a

--- Polaris/dllmain.cpp
+++ Polaris/dllmain.cpp
@@ -24,6 +24,12 @@ SDK::UNetDriver* (*FindNamedNetDriver)(SDK::UEngine*, SDK::UWorld*, SDK::FName) 
 VOID (*SetWorld)(SDK::UNetDriver*, SDK::UWorld*) = nullptr;
 BOOL (*InitListen)(SDK::UNetDriver*, PVOID, SDK::FURL&, BOOL, SDK::FString&) = nullptr;
 SDK::FLevelCollection* (*FindCollectionByType)(SDK::UWorld*, INT) = nullptr;
+VOID (*InitHandler)(SDK::UNetConnection*) = nullptr;
+
+VOID InitHandlerHook(SDK::UNetConnection* what)
+{
+    // init
+}
 
 BOOL Listen()
 {
@@ -45,6 +51,8 @@ BOOL Listen()
 
     if (!(*Core::pWorld)->NetDriver)
         return FALSE;
+
+    printf("%s\n", (*Core::pWorld)->NetDriver->GetFullName().c_str());
 
     SDK::FString Error;
     if (!InitListen((*Core::pWorld)->NetDriver, (*Core::pWorld), Core::pLevel->URL, FALSE, Error))
@@ -79,7 +87,7 @@ DWORD WINAPI Main(LPVOID lpParam)
         ExitProcess(EXIT_FAILURE);
     }
 
-    auto pCreateNamedNetDriverOffset = Util::FindPattern("\xE8\x00\x00\x00\x00\x84\xC0\x75\x43\x80\x3D\x00\x00\x00\x00\x00", "x????xxxxxx?????");
+    /*auto pCreateNamedNetDriverOffset = Util::FindPattern("\xE8\x00\x00\x00\x00\x84\xC0\x75\x43\x80\x3D\x00\x00\x00\x00\x00", "x????xxxxxx?????");
     auto pCreateNamedNetDriverAddress = pCreateNamedNetDriverOffset + 5 + *reinterpret_cast<int32_t*>(pCreateNamedNetDriverOffset + 1);
 
     CreateNamedNetDriver = reinterpret_cast<decltype(CreateNamedNetDriver)>(pCreateNamedNetDriverAddress);
@@ -101,17 +109,27 @@ DWORD WINAPI Main(LPVOID lpParam)
     auto pFindCollectionByTypeOffset = Util::FindPattern("\xE8\x00\x00\x00\x00\x4C\x8B\xF0\x4C\x8B\xFB", "x????xxxxxx");
     auto pFindCollectionByTypeAddress = pFindCollectionByTypeOffset + 5 + *reinterpret_cast<int32_t*>(pFindCollectionByTypeOffset + 1);
 
-    FindCollectionByType = reinterpret_cast<decltype(FindCollectionByType)>(pFindCollectionByTypeAddress);
+    FindCollectionByType = reinterpret_cast<decltype(FindCollectionByType)>(pFindCollectionByTypeAddress);*/
 
     polaris::Console::LogRaw("Welcome to Polaris!", 11);
     /*new polaris::Renderer; // Initialize renderer.
     new polaris::Athena; // TEMP: Initialize Athena.*/
 
-    Util::InitSdk();
+    /*Util::InitSdk();
     Util::InitCore();
 
     if (Listen())
-        printf("listen success");
+        printf("listen success");*/
+
+    auto pProcessEventAddress = Util::FindPattern("\x48\x89\x6C\x24\x00\x48\x89\x74\x24\x00\x57\x48\x83\xEC\x50\x48\x8B\xF9\xB9\x00\x00\x00\x00\xE8\x00\x00\x00\x00\x33\xED\x48\x85\xC0\x74\x0D\x48\x8B\xC8\xE8\x00\x00\x00\x00\x48\x8B\xC8\xEB\x03\x48\x8B\xCD\x48\x8D\xB7\x00\x00\x00\x00\x48\x8D\x44\x24\x00\x48\x3B\xF0\x74\x13\x4C\x8B\x06\x48\x89\x0E\x4D\x85\xC0\x74\x17\x49\x8B\x00\x49\x8B\xC8\xEB\x08\x48\x85\xC9\x74\x0A\x48\x8B\x01\xBA\x00\x00\x00\x00\xFF\x10\x48\x8B\x0E\x48\x85\xC9\x0F\x84\x00\x00\x00\x00\x48\x8B\x47\x58", "xxxx?xxxx?xxxxxxxxx????x????xxxxxxxxxxx????xxxxxxxxxxx????xxxx?xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx????xxxxxxxxxx????xxxx");
+    if (!pProcessEventAddress)
+    {
+        MessageBox(NULL, static_cast<LPCWSTR>(L"Finding pattern for ProcessEvent has failed, please re-open Fortnite and try again!"), static_cast<LPCWSTR>(L"Error"), MB_ICONERROR);
+        ExitProcess(EXIT_FAILURE);
+    }
+
+    MH_CreateHook(static_cast<LPVOID>(pProcessEventAddress), InitHandlerHook, reinterpret_cast<LPVOID*>(&InitHandler));
+    MH_EnableHook(static_cast<LPVOID>(pProcessEventAddress));
 
     return FALSE;
 }
