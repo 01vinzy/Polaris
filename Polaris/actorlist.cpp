d3223346e510c1750ca6f0977369105c118d8201
Cyuubi <cyuubiapps@gmail.com>
2020-09-24 14:37:57 -0400

    new shit ig

--- Polaris/actorlist.cpp
+++ Polaris/actorlist.cpp
@@ -15,52 +15,72 @@ namespace polaris
 		ImGui::Begin("Actors", &bShowWindow, ImGuiWindowFlags_Modal);
 		{
 			// Actor Hierarchy
-			static int selected = 0;
+			static int selectedLevel = 0;
+			static int selectedActor = 0;
 			{
 				ImGui::BeginChild("Hierarchy", ImVec2(300, 0), true);
 				{
-					for (int i = 0; i < Globals::gpLevel->Actors.Num(); i++)
+					for (int i = 0; i < (*polaris::Globals::gpWorld)->Levels.Num(); i++)
 					{
-						// Absolutely make sure the actor exists, Unreal Engine is very prone to crashing
-						if (Globals::gpLevel->Actors.IsValidIndex(i) && Globals::gpLevel->Actors[i])
+						if ((*polaris::Globals::gpWorld)->Levels.IsValidIndex(i) && (*polaris::Globals::gpWorld)->Levels[i])
 						{
-							// Create a button for the actor, and set the selected actor once clicked
-							if (ImGui::Selectable(Globals::gpLevel->Actors[i]->GetName().c_str(), selected == i))
-								selected = i;
+							SDK::ULevel* level = (*polaris::Globals::gpWorld)->Levels[i];
+
+							if (ImGui::TreeNode(level->GetName().c_str()))
+							{
+								for (int j = 0; j < level->Actors.Num(); j++)
+								{
+									// Absolutely make sure the actor exists, Unreal Engine is very prone to crashing
+									if (level->Actors.IsValidIndex(j) && level->Actors[j])
+									{
+										SDK::AActor* actor = level->Actors[j];
+
+										// Create a button for the actor, and set the selected actor once clicked
+										if (ImGui::Selectable(actor->GetName().c_str(), selectedActor == j))
+										{
+											selectedLevel = i;
+											selectedActor = j;
+										}
+									}
+								}
+
+								ImGui::TreePop();
+							}
 						}
 					}
 					ImGui::EndChild();
 				}
 			}
 
-			auto selectedActor = Globals::gpLevel->Actors[selected];
+			auto level = (*polaris::Globals::gpWorld)->Levels[selectedLevel];
+			auto actor = level->Actors[selectedActor];
 
 			// Inspector
 			// Only draw the inspector screen if the selected actor exists
-			if (selectedActor)
+			if (actor)
 			{
 				ImGui::SameLine();
 
 				ImGui::BeginGroup();
 				ImGui::BeginChild("Inspector", ImVec2(0, -ImGui::GetFrameHeightWithSpacing())); // Leave room for 1 line below us
-				ImGui::Text(selectedActor->GetName().c_str());
+				ImGui::Text(actor->GetName().c_str());
 				ImGui::Separator();
 				ImGui::BeginTabBar("##Tabs", ImGuiTabBarFlags_None);
 				{
 					if (ImGui::BeginTabItem("Properties"))
 					{
-						ImGui::TextWrapped("ID: %s", selectedActor->GetFullName().c_str());
+						ImGui::TextWrapped("ID: %s", actor->GetFullName().c_str());
 
 						// Position and Rotation field
-						ImGui::TextWrapped("Position: (%d, %d, %d)", selectedActor->K2_GetActorLocation().X, selectedActor->K2_GetActorLocation().Y, selectedActor->K2_GetActorLocation().Z);
-						ImGui::TextWrapped("Rotation: (%d, %d, %d)", selectedActor->K2_GetActorRotation().Pitch, selectedActor->K2_GetActorRotation().Roll, selectedActor->K2_GetActorRotation().Yaw);
+						ImGui::TextWrapped("Position: (%d, %d, %d)", actor->K2_GetActorLocation().X, actor->K2_GetActorLocation().Y, actor->K2_GetActorLocation().Z);
+						ImGui::TextWrapped("Rotation: (%d, %d, %d)", actor->K2_GetActorRotation().Pitch, actor->K2_GetActorRotation().Roll, actor->K2_GetActorRotation().Yaw);
 
 						// Owner field
-						auto ownerLabel = selectedActor->Owner != nullptr ? selectedActor->Owner->GetName().c_str() : "None";
+						auto ownerLabel = actor->Owner != nullptr ? actor->Owner->GetName().c_str() : "None";
 						ImGui::TextWrapped("Owner: %s", ownerLabel);
 						
 						// Authority field
-						auto authorityLabel = selectedActor->HasAuthority() ? "Yes" : "No";
+						auto authorityLabel = actor->HasAuthority() ? "Yes" : "No";
 						ImGui::TextWrapped("Has Authority: %s", authorityLabel);
 
 						ImGui::EndTabItem();
@@ -70,12 +90,12 @@ namespace polaris
 				ImGui::EndChild();
 
 				if (ImGui::Button("Destroy")) 
-					selectedActor->K2_DestroyActor();
+					actor->K2_DestroyActor();
 
 				ImGui::SameLine();
 
 				if (ImGui::Button("Toggle Authority"))
-					selectedActor->Role = selectedActor->HasAuthority() ? SDK::ENetRole::ROLE_None : SDK::ENetRole::ROLE_Authority;
+					actor->Role = actor->HasAuthority() ? SDK::ENetRole::ROLE_None : SDK::ENetRole::ROLE_Authority;
 
 				ImGui::EndGroup();
 			}
